63da9efa9f463b8c0fc9d3cb72a2b960c77aff2e
chen19940213 <17392255148@163.com>
2020-04-10 17:41:53 +0800

    test

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,220 @@
+<template>
+ 
+ <div >
+   <!-- banner1 -->
+  <div class="headerImg" >
+    <div class="header"></div>
+<!-- <div class="banner1">
+             <div v-for="(imgUrl, index) in bannerList" v-show="index===mark" :key="index" class="slideshow">
+                     <img :src=imgUrl>
+             </div>
+    </div> -->
+<!-- <mt-swipe :auto="2000" >
+  <mt-swipe-item >
+         <img src="../assets/img/banner1.png" alt="logo" class="img1" />
+  </mt-swipe-item>
+  <mt-swipe-item >
+         <img src="../assets/img/banner2.png" alt="logo" class="img1" />
+  </mt-swipe-item>
+  <mt-swipe-item >
+         <img src="../assets/img/banner3.png" alt="logo" class="img1" />
+  </mt-swipe-item>
+ </mt-swipe> -->
+      <!-- <img src="../assets/img/header.png" alt="" /> -->
+      </div> 
+      <!-- {/* submit */} -->
+      <!-- <div >
+      <div  >立即获取产品报价</div>
+      <input type="text" placeholder="请输入姓名"/>
+      <input type="number" placeholder="请输入手机号（必填）"/>
+       <button >免费获取</button>
+       </div> -->
+       <!-- {/* banner2 */} -->
+       <!-- <Carousel autoplay>
+        <div>
+          <div> <img src="../assets/img/inform.png" alt=""/>王先生</div>
+          <div>154****5689</div>
+           <div> <span>51</span>分钟前</div>
+        </div>
+        <div>
+        <div> <img src="../assets/img/inform.png" alt=""/>王先生</div>
+          <div>154****5689</div>
+           <div> <span>51</span>分钟前</div>
+        </div>
+        <div>
+        <div> <img src="../assets/img/inform.png" alt=""/>王先生</div>
+          <div>154****5689</div>
+           <div> <span>51</span>分钟前</div>
+        </div>
+      </Carousel> -->
+      <!-- {/* info */} -->
+      <!-- <div>
+        <img src="../assets/img/left.png" alt=""/>
+          <span>公司简介</span>
+        <img src="../assets/img/right.png" alt=""/>
+      </div>
+      <div>
+      中山市鸿臣电子塑料有限公司创建于2011年，位于广东省中山市北部的著名经济强镇小榄镇，是一家集生产、加工、销售、服务于一体的私营企业，拥有完整、科学的质量管理体系。生产业务以专用线束生产和注塑加工为主。其主要应用于家用电器、汽车电子、玩具等产品。 公司秉承“品质第一，服务至上”的经营理念，坚持“客户第一”的原则，为广大客户提供优质的服务；以严格的品质要求，为客户提供最优质的产品服务；以稳固、发展、高新、团结和创新的精神，尊重人才，注重技术。 经过9年多的努力和发展，现今已具有一定的规模和实力，拥有一支朝气蓬勃，技术过硬且创新敬业的新生代高素质人才队伍。依靠良好的信誉和技术优势，取得了用户的一致好评及同行的共同认可。 未来，鸿臣将一如既往地提供精准服务和精准交付的高质产品，服务所有客户，帮助客户完成价值，与客户共同创业，共享成果。
+      </div> -->
+      <!-- {/* infoimg */} -->
+      <!-- <div>
+        <img src="../assets/img/infoimg1.png" alt=""/>
+        <img src="../assets/img/infoimg2.png" alt=""/>
+      </div> -->
+      <!-- {/* product */} -->
+      <!-- <div>
+        <img src="../assets/img/left.png" alt=""/>
+          <span>产品展示</span>
+        <img src="../assets/img/right.png" alt=""/>
+      </div> -->
+      <!-- {/* productinfo */} -->
+         <!-- <ul>
+           <li>
+             <img src="../assets/img/product2.png" alt=""/>
+             <p>显示板线材</p>
+           </li>
+           <li>
+             <img src="../assets/img/product2.png" alt=""/>
+             <p>通信线</p>
+           </li>
+           <li>
+             <img src="../assets/img/product3.png" alt=""/>
+             <p>声控板线材</p>
+           </li>
+           <li>
+             <img src="../assets/img/product4.png" alt=""/>
+             <p>驱动板线材</p>
+           </li>
+           <li>
+             <img src="../assets/img/product5.png" alt=""/>
+             <p>开关连接线</p>
+           </li>
+           <li> -->
+             <!-- <img src="../assets/img/product6.png" alt=""/> -->
+             <!-- <p>火线反馈线</p>
+           </li>
+           <li>
+             <img src="../assets/img/product7.png" alt=""/>
+             <p>点火器配套线</p>
+           </li>
+           <li>
+             <img src="../assets/img/product8.png" alt=""/>
+             <p>比列阀连接线</p>
+           </li>
+         </ul> -->
+         <!-- {/* callphone */} -->
+          <!-- <div>
+            <img src="../assets/img/contact1.png" alt=""/>
+          </div> -->
+          <!-- {/* address */} -->
+         
+          <!-- <div>
+        <img src="../assets/img/left.png" alt=""/>
+          <span>联系我们</span>
+        <img src="../assets/img/right.png" alt=""/>
+      </div> -->
+        <!-- {/* contactinfo */} -->
+        <!-- <div>
+           <p>中山市鸿臣电子塑料有限公司</p>
+           <p>联系人：李先生</p>
+           <p>固 话：（070）22230669</p>
+           <p>电 话：18948858136</p>
+           <p>Q Q：1398577897</p>
+           <p>地 址：中山市小榄镇绩东一民安南路137号亿利 B楼四楼</p>
+        </div> -->
+         <!-- {/* submit2 */} -->
+      <!-- <div>立即获取产品报价</div>
+      <input type="text" placeholder="请输入姓名"/>
+      <input type="number" placeholder="请输入手机号（必填）"/>
+       <button>免费获取</button> -->
+        <!-- {/* banner3 */} -->
+        <!-- <Carousel autoplay>
+        <div>
+          <div> <img src="../assets/img/inform.png" alt=""/>王先生</div>
+          <div>154****5689</div>
+           <div> <span>51</span>分钟前</div>
+        </div>
+        <div>
+        <div> <img src="../assets/img/inform.png" alt=""/>王先生</div>
+          <div>154****5689</div>
+           <div> <span>51</span>分钟前</div>
+        </div>
+        <div>
+        <div> <img src="img/inform.png" alt=""/>王先生</div>
+          <div>154****5689</div>
+           <div> <span>51</span>分钟前</div>
+        </div>
+      </Carousel> -->
+      <!-- {/* callphone2 */} -->
+      <!-- <div>
+      <a href="tel:17392255148">
+        <img src="../assets/img/contact1.png" alt=""/>
+        </a>
+      </div> -->
+      </div>
+      
+</template>
+<script>
+ import {Swipe, SwipeItem} from 'mint-ui'
+ import 'mint-ui/lib/style.css'
+ export default {
+ components: {
+  'mt-swipe': Swipe,
+  'mt-swipe-item': SwipeItem
+ },
+ 
+ data () {
+  return {
+        mark: 0, 
+        bannerList:[
+          require('../assets/img/banner1.png'),
+          require('../assets/img/banner2.png'),
+          require('../assets/img/banner3.png'),
+         
+        ] ,
+  }
+ },
+ // 轮播
+  methods:{
+    autoPlay () {  
+      this.mark++;  
+      if (this.mark === 3) { 
+        this.mark = 0  
+      }  
+    },  
+    play () {  
+      setInterval(this.autoPlay, 2000)  
+    },  
+    change (i) {  
+      this.mark = i  
+    }  
+  },
+  created () {  
+    this.play()  
+  },
+  // 轮播
+//  methods:{
+// clickme(){
+//   this.$http.get('https://cnodejs.org/api/v1/topics',{
+//       params: {                           
+//         page: this.page,
+//         limit: this.limit,
+//       },
+//     }).then(res => {                   
+//       this.items = res.data.data;
+//       console.log(this.items);   
+//     }).catch(err => {                 
+//       console.log(err)
+//     })
+// }
+//  }
+
+ }
+</script>
+<style scoped>
+.headerImg .header{
+  width:100vw;
+height:191px;
+background: red;
+}
+</style>
